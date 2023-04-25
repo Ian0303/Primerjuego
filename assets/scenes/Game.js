deee198a30dd61367fc173d7ff206e87cfcb66b8
Denise <deniserey20@hotmail.comm>
2023-04-25 20:36:57 -0300

    Version 0.1

--- assets/scenes/Game.js
+++ assets/scenes/Game.js
@@ -15,6 +15,7 @@ export default class Game extends Phaser.Scene{
         this.load.image("square", "./assets/images/Square.png");
         this.load.image("diamond", "./assets/images/Diamond.png");
         this.load.image("triangle", "./assets/images/Triangle.png");
+        this.load.image("moon", "./assets/images/moon4.png");
 
 
 
@@ -24,25 +25,73 @@ export default class Game extends Phaser.Scene{
         //add background
         this.add.image(400, 300, "sky").setScale(0.555);
 
+        this.add.image(750,50, "moon").setScale(.2)
+
         //add static platforms group
-        let plataforms = this.physics.add.staticGroup();
-        plataforms.create(400, 568, "ground").setScale(2).refreshBody();
+        let platforms = this.physics.add.staticGroup();
+        platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
         //
         this.shapesGroup = this.physics.add.group();
-        this.shapesGroup.create(100, 0, 'diamond');
-        this.shapesGroup.create(200, 0, 'triangle');
-        this.shapesGroup.create(300, 0, 'square');
+        // this.shapesGroup.create(100, 0, 'diamond');
+        // this.shapesGroup.create(200, 0, 'triangle');
+        // this.shapesGroup.create(300, 0, 'square');
 
+        //create event to add shapes
+        this.time.addEvent({
+            delay: 1500,
+            callback: this.addShape,
+            callbackScope: this,
+            loop: true,
+        })
 
         //
         //
         this.player = this.physics.add.sprite(100, 450, "ninja");
         this.player.setCollideWorldBounds(true);
 
+        //create cursors
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        //colliders
+        this.physics.add.collider(this.player, platforms);
+        this.physics.add.collider(this.player, this.shapesGroup);
+        this.physics.add.collider(platforms, this.shapesGroup);
+
+     
+
     }
 
-    uptdate(){
+    update(){
+
+        //movement player
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-250);
+        } else 
+        if (this.cursors.right.isDown) {
+            this.player.setVelocityX(250);
+        } else {
+            this.player.setVelocityX(0);
+        }
+
+        //player jump
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
+
+    }
+
+    addShape() {
+    //get random shape
+    const randomShape = Phaser.Math.RND.pick(['diamond', 'square', 'triangle']);
+
+    //get random position x
+    const randomX = Phaser.Math.RND.between(0, 800);
+
+    //add shape to screen
+    this.shapesGroup.create(randomX, 0, randomShape);
+
+    console.log("shape is added", randomX, randomShape);
 
     }
 
